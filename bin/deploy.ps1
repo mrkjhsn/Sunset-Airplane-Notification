9a51289f3cdd9152ee5e7998e3ca73898f2ef6df
Mark Johnson <mrk.jhsn@gmail.com>
2021-11-09 21:31:05 -0700

    fixed
    added parameter speed up deployment time
    updated python library install format
    added apt dependencies in installer

--- bin/deploy.ps1
+++ bin/deploy.ps1
@@ -1,19 +1,28 @@
-
+param(
+    $pip=$true
+    ,$apt=$true   
+)
 
 # create destination folder
 # copy code into folder
 # copy cron config to apprpriate destination
 $ip_address = '192.168.9.191'
-get-content .\requirements.txt | ForEach-Object{
-    $library = $_
-    $cmd = "/usr/bin/pip install $library"
-    ssh pi@$ip_address $cmd
-}
 
 # login and create folder
 ssh pi@$ip_address mkdir -p 'flightwatch'
 
 # copy src files
 scp src/* pi@${ip_address}:/home/pi/flightwatch
+
+# install utilities
+if ($apt) {
+    ssh pi@$ip_address "sudo apt install at libatlas-base-dev"
+}
+
+# install python libraries
+if ($pip) {
+    $cmd = "/usr/bin/pip3 install -r ./flightwatch/requirements.txt"
+    ssh pi@$ip_address $cmd
+}
 
 ssh pi@$ip_address "echo '0 12 * * * /usr/bin/python3 /home/pi/flightwatch/scheduler.py' | /usr/bin/crontab -"
